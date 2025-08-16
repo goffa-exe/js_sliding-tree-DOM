169573f49d0e66eb396da5d954dad2c30943320a
gOFFa <n4v4rm0re666@gmail.com>
2025-08-17 01:43:27 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+document.addEventListener('click', (e) => {
+  const span = e.target.closest('span');
+
+  if (!span) {
+    return;
+  }
+
+  if (span.nextElementSibling.style.display === 'none') {
+    span.nextElementSibling.style.display = '';
+  } else {
+    span.nextElementSibling.style.display = 'none';
+  }
+});
